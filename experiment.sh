a4267f297a0d5f5cad3bcd4bbaf3e56704abfd58
Dongjin Seo <65718383+dongjin-seo2020@users.noreply.github.com>
2025-03-26 11:11:25 -0400

    Update experiment.sh
    model, log path

--- experiment.sh
+++ experiment.sh
@@ -10,8 +10,8 @@ do
         do
             MODEL_FLAGS="--dropout 0.1 --class_cond True --gray_imgs True"
             DIFF_FLAGS="--learn_sigma True --diffusion_steps 1000 --noise_schedule cosine"
-            DIR_FLAGS="--model_path /home/user/dongjin/1.bending_waveguide/models/wg-64_class-cond/ema_0.9999_025000.pt \
-                        --log_dir /home/user/dongjin/1.bending_waveguide/logs/wg-64_class-cond/model_025000/sim-guided/${prop}_tsr=${tsr}_class=${manual_class_id}_eta=${eta}"
+            DIR_FLAGS="--model_path YOUR_MODEL_PATH \
+                        --log_dir YOUR_LOG_PATH/sim-guided/${prop}_tsr=${tsr}_class=${manual_class_id}_eta=${eta}"
             SAMPLE_FLAGS="--batch_size 1 --num_samples 1 --timestep_respacing ${tsr} --num_classes 3 --manual_class_id ${manual_class_id} --gpu_id ${gpu_id} --save_img False"
             SIM_FLAGS="--sim_guided True --sim_type waveguide  --use_normed_grad True --use_adjgrad_norm False  --eta ${eta} --prop_dir ${prop} --save_inter True --interval 1"
             echo -e "\n\n\n\n############################ Sampling with eta = ${eta} ##############################\n"
